2966fd0118ab6be339fdeedd37402cbf3a46f217
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-10-09 19:54:49 +0100

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-function that returns a list of lists of integers representing 
+function that returns a list of lists of integers representing
 the Pascal’s triangle of n
 """
 
