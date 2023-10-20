13d65c9f7fed319c4838e827fe446c840ac70c35
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-10-20 07:53:29 +0100

    fix bug

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,27 +5,26 @@ result in exactly n H characters in the file.
 """
 
 
-def minOperations(n):
+def minOperations(n: int) -> int:
     """
     a method that calculates the fewest number of operations needed
     to result in exactly n H characters in the file.
     """
-    if not isinstance(n, int):
+    if n <= 0:
         return 0
+    # No operation has been file_text yet
     ops_count = 0
+    # Assume nothing has been copied to the clipboard yet
     clipboard = 0
-    done = 1
+    # Assume the file contain 1 alphabet (H)
+    file_text = 1
     # print ('H', end='')
-    while done < n:
-        if clipboard == 0:
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-        elif n - done > 0 and (n - done) % done == 0:
-            clipboard = done
-            done += clipboard
-            ops_count += 2
-        elif clipboard > 2:
-            done += clipboard
+    while file_text < n:
+        # copy all when the length is divisible by n
+        if n % file_text == 0:
+            clipboard = file_text
             ops_count += 1
+        # paste all the time. Action 2
+        file_text += clipboard
+        ops_count += 1
     return ops_count
