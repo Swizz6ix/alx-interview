0753548be5f51d996584d096f83958f8054c7578
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-10-20 08:06:09 +0100

    fix bug

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,15 +5,14 @@ Lockboxes that contains many keys
 
 def canUnlockAll(boxes):
     """Keys for the lockboxes"""
-
-    def dfs(box_idx, visited):
-        visited.add(box_idx)
-        for key in boxes[box_idx]:
-            if key not in visited:
-                dfs(key, visited)
-
-    n = len(boxes)
-    visited = set()
-    dfs(0, visited)
-
-    return len(visited) == n
+    box_len = len(boxes)
+    found_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        box_idx = unseen_boxes.pop()
+        if not box_idx or box_idx >= box_len or box_idx < 0:
+            continue
+        if box_idx not in found_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[box_idx])
+            found_boxes.add(box_idx)
+    return box_len == len(found_boxes)
