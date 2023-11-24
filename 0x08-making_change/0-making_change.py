297337ab2520cdb5e4d5ce4f9a1d422ca4983a09
swizz6ix <ferdinandcharles6@gmail.com>
2023-11-24 18:52:00 +0100

    interview

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+change making script
+"""
+
+
+def makeChange(coins, total):
+    """
+    determine the fewest number of coins needed to meet a given amount total.
+    """
+    if total <= 0:
+        return 0
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while total > 0:
+        # If total cannot be met by any number of coins you have, return -1
+        if coin_idx >= n:
+            return -1
+        if total - sorted_coins[coin_idx] >= 0:
+            total -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
