09bac6c94118b1fbde797b4ad781ec6c45bfc483
swizz6ix <ferdinandcharles6@gmail.com>
2023-12-03 20:37:48 +0100

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api-hbtn.io/api/films/' + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, (error, response, body) => {
   if (!error) {
     const characters = JSON.parse(body).characters;
@@ -12,6 +12,7 @@ request(url, (error, response, body) => {
 const printOrderly = (characters, index) => {
   request(characters[index], (error, response, body) => {
     if (!error) {
+      console.log(JSON.parse(body).name);
       if (index + 1 < characters.length) {
         printOrderly(characters, index + 1);
       }
